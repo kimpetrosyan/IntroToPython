2c0e7ca9039ea5cc158e81a9c16608e7101820b6
Kim Petrosyan <kimss001@mail.ru>
2020-03-07 12:37:15 +0400

    second commit

---
+++ Week1/Practical/Problem2.py
@@ -0,0 +1,9 @@
+course = "Intro to Python"
+student  = "Kim Petrosyan"
+nickname = "student"
+grade = "1"
+python_expert = True
+print("course:",course)
+print("student:",student)
+print("nickname:",nickname)
+print("grade:",grade)
---
+++ Week1/Practical/Problem4.py
@@ -0,0 +1,5 @@
+AB = 3
+AC = 4
+BC =  (AB**2 + AC**2)**(1/2)
+print (BC)
+print ("The hypotenuse of the triangle ABC =",BC)
